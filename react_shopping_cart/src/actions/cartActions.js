59a962b24c35155aa1bc7848081e34fbb6a102ba
LI_XE <hyangeun312@gmail.com>
2021-12-20 23:55:15 -0600

    add-cart-redux

--- react_shopping_cart/src/actions/cartActions.js
+++ react_shopping_cart/src/actions/cartActions.js
@@ -20,9 +20,7 @@ export const addToCart = (product) => (dispatch, getState) => {
 };
 
 export const removeFromCart = (product) => (dispatch, getState) => {
-  const cartItems = getState()
-    .cart.cartItems.slice()
-    .filter((x) => x._id !== product._id);
+  const cartItems = getState().cart.cartItems.slice().filter((x) => x._id !== product._id);
   dispatch({ type: REMOVE_FROM_CART, payload: { cartItems } });
   localStorage.setItem("cartItems", JSON.stringify(cartItems));
 };
